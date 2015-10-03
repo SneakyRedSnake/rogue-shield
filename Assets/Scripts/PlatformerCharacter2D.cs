9c5ed7e640d9521360ad50bc5cabd6216ea5ed7b
Lydwen <tomlau94@aol.com>
2015-10-03 01:12:18 -0400

    Initial project
    Issue #1 - We have a new project, with two little sprit (a red square and a black square). There is also a TestScene and a few prefab (One for the floor and one for a character). I add two little script : one for the controls and another one with a class which represents a character in Unity.

---
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformerCharacter2D : MonoBehaviour 
+{
+	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
+	
+	void Awake()
+	{
+
+	}
+	
+	void Update()
+	{
+
+	}
+	
+	void FixedUpdate()
+	{
+
+	}
+	
+	
+	public void Move(float move)
+	{
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+	}
+}

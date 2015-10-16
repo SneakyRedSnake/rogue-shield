1843852a83802b91fc54a85049dceffc411ab819
Lydwen <tomlau94@aol.com>
2015-10-16 18:08:59 -0400

    The shield can repulse enemies now
    Issue #6 - Add the Repulse script : when sth with a predefined layer hit the gameobject which has this component, it will be repulsed with a predefined force.

---
+++ Assets/Scripts/Repulse.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 	Component which permits an object to repulse selected objects
+/// 	with a given force
+/// </summary>
+public class Repulse : MonoBehaviour {
+	[SerializeField]LayerMask whatCanRepulse;		//what the shield can repulse
+	[SerializeField]Vector2 repulseForce;			//the force of the repulsion
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// 	When something hit this and can be repulsed,
+	/// 	we repulse it.
+	/// </summary>
+	/// <param name="other">The collider2D of the object which collides with this</param>
+	void OnTriggerEnter2D(Collider2D other){
+		//we check if we can repulse the other gameObject
+		if ((whatCanRepulse.value & 1<<other.gameObject.layer)>0){
+			other.gameObject.rigidbody2D.AddForce(repulseForce);
+		}
+	}
+}

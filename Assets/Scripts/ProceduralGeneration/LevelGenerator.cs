1035373558ecce3b592493808726ef97af1f73e9
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-10-24 15:11:48 -0400

    Story #3 Enhanced level generation
    - Now more stable
    - Added level abstraction level class composed of rooms
    - The path from spawn to end is now open (no walls)

--- Assets/Scripts/ProceduralGeneration/LevelGenerator.cs
+++ Assets/Scripts/ProceduralGeneration/LevelGenerator.cs
@@ -1,149 +1,110 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 
-public class LevelGenerator : MonoBehaviour
+namespace Procedural
 {
-	[Tooltip("Put here Wall Prefab ! ")]
-	public Transform wall;
-	public int minimumDistance = 6;
-	public int maximumDistance = 10;
-	public Vector2 endPosition = new Vector2(6, 6);
-	public int levelWidth = 10;
-	public int levelHeight = 10;
-	
-	private Room[,] level;
-    private List<Vector2> layout;
+	public class LevelGenerator : MonoBehaviour
+	{
+		[Tooltip("Put here Wall Prefab ! ")]
+		public Transform wall;
+		public int minimumDistance = 6;
+		public int maximumDistance = 10;
+		public Vector2 startingPosition = new Vector2 (0, 0);
+		public Vector2 endPosition = new Vector2 (6, 6);
+		public int levelWidth = 10;
+		public int levelHeight = 10;
+		public LevelGenerationStrategy levelGenerationStrategy;
 
-	private Transform environment; 				// Stores the roo object for the environment
+		private Transform environment; 				// Stores the roo object for the environment
+		// Use this for initialization
+		void Start ()
+		{
+			int nbIter = 0;
+			float elapsedTime = Time.realtimeSinceStartup;
 
-    enum Facing
-    {
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST
-    }
+			GameObject obj = new GameObject ("Environment");
+			environment = obj.GetComponent<Transform> ();
 
-    // Use this for initialization
-    void Start()
-    {
-		int nbIter = 0;
-		float elapsedTime = Time.realtimeSinceStartup;
+			levelGenerationStrategy = new RecursiveGeneration (levelWidth, levelHeight, startingPosition, endPosition, minimumDistance, maximumDistance);
+			Level level = levelGenerationStrategy.generateLevel ();
 
-		GameObject obj = new GameObject ("Environment");
-		environment = obj.GetComponent<Transform> ();
-
-        level = new Room[levelWidth, levelHeight];
-		layout = new List<Vector2> (10);
-		while (findPath(0, 0, minimumDistance) == false) {
-			nbIter ++;
-			if(nbIter > 2) {
-				Debug.Log("Path not found");
-				break;
-
-			}
+			foreach (Vector2 v in level.Layout) {
+				generateRoom (level.Rooms[(int)v.x, (int)v.y]);
+			}		
 		}
 
-		elapsedTime = Time.realtimeSinceStartup - elapsedTime;
-		Debug.Log ("Number of iteration : " + nbIter);
-		Debug.Log ("Generation time : " + elapsedTime);
-        Debug.Log ("Number of Rooms : " + layout.Count);
-		Debug.Log ("Rooms Layout: ");
-		foreach(Vector2 v in layout) {
-			Debug.Log(v);
-			generateSquare(10, 10, level[(int)v.x, (int)v.y].getPosition() * 10);
+		public void instanciateWall (Vector2 position)
+		{
+			Transform wallInstance = (Transform)Instantiate (wall, new Vector3 (position.x, position.y, 0), Quaternion.identity);
+			wallInstance.localScale = new Vector2 (0.5f, 0.5f);
+			wallInstance.SetParent (environment);
 		}
+
+		/**
+		 * Generate a bordered square (not filled) beginning at position
+		 */
+		private void generateSquare (int width, int height, Vector2 position)
+		{
 		
-    }
-
-    private bool findPath(int x, int y, int minDistance)
-    {
-        Debug.Log("Starting find Path : x = " + x +"   y = " + y + "    distance = " + minDistance);
-
-        if ((x == endPosition.x && y == endPosition.y) && (minDistance <= 0) && (minDistance > -(maximumDistance - minimumDistance))) {
-			addRoomToLayout (x, y);
-			return true;
-		}
-            
-        if (!validPosition(x, y))
-            return false;
-
-		addRoomToLayout (x, y);
-
-        Facing r = (Facing)Random.Range(0, 4);
-        Debug.Log("r : " + r);
-        switch (r)
-        {
-            case Facing.NORTH:
-                if (findPath(x, y + 1, minDistance - 1) == true) return true; 
-				goto case Facing.SOUTH;
-
-            case Facing.SOUTH:
-                if (findPath(x, y - 1, minDistance - 1) == true) return true;
-				goto case Facing.EAST;
-                
-            case Facing.EAST:
-                if (findPath(x + 1, y, minDistance - 1) == true) return true;
-				goto case Facing.WEST;
-
-            case Facing.WEST:
-                if (findPath(x - 1, y, minDistance - 1) == true) return true;
-                break;
-
-            default:
-                Debug.Log("Error");
-                break;
-        }
-        layout.RemoveAt(layout.Count - 1);
-		level [x, y] = null;
-        return false;
-    }
-
-	private void addRoomToLayout(int x, int y) {
-		layout.Add(new Vector2(x, y));
-		level [x, y] = new Room (new Vector2 (x, y));
-	}
-
-    private bool validPosition(int x, int y)
-    {
-		foreach(Vector2 position in layout) {
-				if(position.x == x && position.y == y)
-					return false;
-		}
-        return (((x >= 0) && (x < levelWidth)) && ((y >= 0) && (y < levelHeight)));
-    }
-
-
-	private void instanciateWall(Vector2 position) {
-		Transform wallInstance = (Transform) Instantiate(wall, new Vector3(position.x, position.y, 0), Quaternion.identity);
-		wallInstance.localScale = new Vector2 (0.5f, 0.5f);
-		wallInstance.SetParent(environment);
-	}
-
-	/**
-	 * Generate a bordered square (not filled) beginning at position
-	 */
-	private void generateSquare(int width, int height, Vector2 position) {
-		
-		for (int i = 0; i <= width; i++) {
-			for (int j = 0; j <= height; j++) {
-				if(i == 0 || i == width || j == 0 || j == height) {
+			for (int i = 0; i <= width; i++) {
+				for (int j = 0; j <= height; j++) {
+					if (i == 0 || i == width || j == 0 || j == height) {
 					
-					Transform wallInstance = (Transform) Instantiate(wall, new Vector3(i + position.x, j + position.y, 0), Quaternion.identity);
-					wallInstance.SetParent(environment);
+						Transform wallInstance = (Transform)Instantiate (wall, new Vector3 (i + position.x, j + position.y, 0), Quaternion.identity);
+						wallInstance.SetParent (environment);
+					}
 				}
 			}
 		}
-	}
-	
-	/**
-	 * Generate Horizontal platform beginning at position
-	 */ 
-	private void generatePlatform(int width, Vector2 position) {
+
+		private void generateRoom (Room room)
+		{
+			int width = room.Width;
+			int height = room.Height;
+			Vector2 position = room.getPosition ();
+
+			foreach (Facing facing in room.getFacings()) {
+				int i, j;
+				i = j = 0;
+
+				switch (facing) {
+				case Facing.EAST:
+					i = width;
+					goto case Facing.WEST;
+
+				case Facing.WEST:
+					for (j = 0; j < height; j++) {
+						Transform wallInstance = (Transform)Instantiate (wall, new Vector3 (i + position.x, j + position.y, 0), Quaternion.identity);
+						wallInstance.SetParent (environment);
+					}
+					break;
+
+				case Facing.NORTH:
+					j = height;
+					goto case Facing.SOUTH;
+				
+				case Facing.SOUTH:
+					for (i = 0; i < width; i++) {
+						Transform wallInstance = (Transform)Instantiate (wall, new Vector3 (i + position.x, j + position.y, 0), Quaternion.identity);
+						wallInstance.SetParent (environment);
+					}
+					break;
+
+				}
+			}
+		}
+
+		/**
+		 * Generate Horizontal platform beginning at position
+		 */ 
+		private void generatePlatform (int width, Vector2 position)
+		{
 		
-		for (int i = 0; i <= width; i++) {
-			Transform wallInstance = (Transform) Instantiate(wall, new Vector3(i + position.x, position.y, 0), Quaternion.identity);
-			wallInstance.SetParent(environment);
+			for (int i = 0; i <= width; i++) {
+				Transform wallInstance = (Transform)Instantiate (wall, new Vector3 (i + position.x, position.y, 0), Quaternion.identity);
+				wallInstance.SetParent (environment);
+			}
 		}
 	}
 }
--- Assets/Scripts/ProceduralGeneration/Room.cs
+++ Assets/Scripts/ProceduralGeneration/Room.cs
@@ -1,30 +1,51 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Room
+namespace Procedural
 {
-    enum Facing
-    {
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST
-    }
+	public class Room
+	{
+		private Vector2 position;
+		private int width = 10;
+		private int height = 10;
+		private HashSet<Facing> facings;
 
-	private Vector2 position;
-    private int width = 40;
-    private int height = 40;
-    private HashSet<Facing> facings;
+		public Room (Vector2 position)
+		{
+			this.position = position;
+			facings = new HashSet<Facing> ();
+			foreach (Facing facing in Enum.GetValues(typeof(Facing))) {
+				facings.Add (facing);
+			}
+		}
 
-    public Room(Vector2 position)
-    {
-		this.position = position;
-    }
+		public Vector2 getPosition ()
+		{
+			return position;
+		}
 
-	public Vector2 getPosition() {
-		return position;
+		public void removeFacing (Facing facing)
+		{
+			facings.Remove (facing);
+		}
+
+		public HashSet<Facing> getFacings ()
+		{
+			return facings;
+		}
+
+		public int Width {
+			get {
+				return this.width;
+			}
+		}
+
+		public int Height {
+			get {
+				return this.height;
+			}
+		}
 	}
-
-
 }

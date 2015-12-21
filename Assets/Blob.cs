85dc8ebfd5d74bd651bd897f4c8a7338ed4246bf
Fabien VICENTE <fvicente@polytech.unice.fr>
2015-12-21 17:22:55 -0500

    added a new enemy: a green blob

---
+++ Assets/Blob.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blob: AbstractKillable {
+	[SerializeField] GameObject blobPrefab;
+	[SerializeField] float healthCuttoff = 50;
+	HealthBehavior health;
+	float maxHealth;
+	float damages;
+	// Use this for initialization
+	void Start () {
+		health = GetComponent<HealthBehavior>();
+		maxHealth = health.GetMaxHealth ();
+		damages = GetComponent<BaseDamageDealer>().damages;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public override void Kill() {
+		if (maxHealth >= healthCuttoff) {
+			GameObject blob = (GameObject)Instantiate (blobPrefab, transform.position + transform.right, transform.rotation);
+			GameObject blob2 = (GameObject)Instantiate (blobPrefab, transform.position - transform.right, transform.rotation);
+			blob.transform.localScale /= 2;
+			blob2.transform.localScale /= 2;
+			blob.GetComponent<HealthBehavior> ().setMaxHealth (maxHealth / 2);
+			blob2.GetComponent<HealthBehavior> ().setMaxHealth (maxHealth / 2);
+			blob.GetComponent<BaseDamageDealer>().damages = damages/2;
+			blob2.GetComponent<BaseDamageDealer>().damages = damages/2;
+		}
+		Destroy (gameObject);
+	}
+}
